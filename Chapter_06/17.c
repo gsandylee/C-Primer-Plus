2f5a3accd16709eed400ad2232f9aa69e5879a95
gsandylee <31379177+gsandylee@users.noreply.github.com>
2020-12-23 13:18:49 +0800

    Update 17.c

--- Chapter_06/17.c
+++ Chapter_06/17.c
@@ -6,9 +6,15 @@ int main(void)
     int year;
     double money;
     
-    for(year = 1, money=INITIAL_MONEY; money>0; year++)
+    for(year = 1, money=INITIAL_MONEY; money>=10; year++)
         money += money * ANNUAL_RATE - 10;			//计算每年年终的账户余额
+    
+    money += money * ANNUAL_RATE;//最后余额先结息
+    
+    for (; money >= 10; year++)//最后余额结息超10万
+        money += money * ANNUAL_RATE - 10;
+    
     printf("After %d years, Chuckie will draw all money from his account.\n", year);
-
+    printf("%f\n",money);//最后一次取款金额不会为负数。
     return 0;
 }
